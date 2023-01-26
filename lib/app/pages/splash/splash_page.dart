a173deb073a97cf4e625fde1e07f74516f58a292
João Paulo Camargo <joaopaulobrasiliano@gmail.com>
2023-01-26 08:42:13 -0300

    feat: create splash page layout

--- lib/app/pages/splash/splash_page.dart
+++ lib/app/pages/splash/splash_page.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/material.dart';
+import 'package:delivery_app/app/core/ui/helpers/size_extensions.dart';
 import 'package:delivery_app/app/core/ui/widgets/delivery_button.dart';
+import 'package:flutter/material.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
@@ -7,18 +8,38 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Splash Page'),
-      ),
-      body: Center(
-        child: Column(
+      body: ColoredBox(
+        color: const Color(0xFF140E0E),
+        child: Stack(
           children: [
-            DeliveryButton(
-              label: 'Teste',
-              onPressed: () {},
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: SizedBox(
+                width: context.screenWidth,
+                child: Image.asset(
+                  'assets/images/lanche.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
             ),
-            TextFormField(
-              decoration: const InputDecoration(label: Text('Test')),
+            Center(
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: context.percentHeight(.3),
+                  ),
+                  Image.asset('assets/images/logo.png'),
+                  const SizedBox(
+                    height: 80,
+                  ),
+                  DeliveryButton(
+                    height: 40,
+                    width: context.percentWidth(.6),
+                    label: 'ACESSAR',
+                    onPressed: () {},
+                  ),
+                ],
+              ),
             )
           ],
         ),
