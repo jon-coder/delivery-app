b81cdec495002c8c81ae062a04bb045d6fb83cee
João Paulo Camargo <joaopaulobrasiliano@gmail.com>
2023-01-28 12:40:05 -0300

    feat: define HomePage base with DevileryProductTile

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -1,16 +1,42 @@
+import 'package:delivery_app/app/core/ui/helpers/loader.dart';
+import 'package:delivery_app/app/core/ui/helpers/messages.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+import 'package:delivery_app/app/core/ui/widgets/delivery_appbar.dart';
+import 'package:delivery_app/app/pages/home/widgets/delivery_product_tile.dart';
+import 'package:delivery_app/app/models/product_model.dart';
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with Loader, Messages {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
-      ),
-      body: const Center(
-        child: Text('HOME PAGE'),
+      appBar: DeliveryAppbar(),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                return DeliveryProductTile(
+                  product: ProductModel(
+                    id: 0,
+                    name: 'Lanche X',
+                    description: 'Lanche acompanha pão francês, contra filé, alface, vinagrete, queijo e maionese.',
+                    price: 22.0,
+                    image: 'https://assets.unileversolutions.com/recipes-v2/106684.jpg?imwidth=800',
+                  ),
+                );
+              },
+            ),
+          )
+        ],
       ),
     );
   }
