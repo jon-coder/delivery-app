daec2253e5588aee423cfe03fb39e38c67f3070f
João Paulo Camargo <joao.camargo@willbank.com.br>
2023-04-12 10:25:37 -0300

    feat: create ShoppingBagWidget

---
+++ lib/app/pages/home/widgets/shopping_bag_widget.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+import 'package:delivery_app/app/core/extensions/formatter_extensions.dart';
+import 'package:delivery_app/app/core/ui/helpers/size_extensions.dart';
+import 'package:delivery_app/app/core/ui/styles/text_styles.dart';
+import 'package:delivery_app/app/dto/order_product_dto.dart';
+
+class ShoppingBagWidget extends StatelessWidget {
+  final List<OrderProductDto> bag;
+
+  const ShoppingBagWidget({
+    super.key,
+    required this.bag,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    var totalBag = bag.fold(0.0, (total, element) => total += element.totalPrice).currencyPtBR;
+
+    return Container(
+      width: context.screenWidth,
+      height: 90,
+      padding: const EdgeInsets.all(18),
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black26,
+            blurRadius: 5,
+          ),
+        ],
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(12),
+          topRight: Radius.circular(12),
+        ),
+      ),
+      child: ElevatedButton(
+        onPressed: () {},
+        child: Stack(
+          children: [
+            const Align(
+              alignment: Alignment.centerLeft,
+              child: Icon(
+                Icons.shopping_cart_outlined,
+              ),
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Text(
+                'Ver Sacola',
+                style: context.textStyles.textExtraBold.copyWith(fontSize: 14),
+              ),
+            ),
+            Align(
+              alignment: Alignment.centerRight,
+              child: Text(
+                totalBag,
+                style: context.textStyles.textExtraBold.copyWith(fontSize: 12),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
