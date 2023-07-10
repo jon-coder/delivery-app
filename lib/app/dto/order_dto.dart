c9129b2d8f43c9708e30596c85e599dfd9ef216b
João Paulo Camargo <joao.camargo@willbank.com.br>
2023-07-10 14:36:20 -0300

    refact: adjustment to relative imports in dtos

--- lib/app/dto/order_dto.dart
+++ lib/app/dto/order_dto.dart
@@ -1,6 +1,4 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-
-import 'package:delivery_app/app/dto/order_product_dto.dart';
+import 'order_product_dto.dart';
 
 class OrderDto {
   final List<OrderProductDto> products;
--- lib/app/dto/order_product_dto.dart
+++ lib/app/dto/order_product_dto.dart
@@ -1,5 +1,4 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'package:delivery_app/app/models/product_model.dart';
+import '../models/product_model.dart';
 
 class OrderProductDto {
   final ProductModel product;
